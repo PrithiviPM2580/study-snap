a730737031c2851905979350e5c7c38f858313a1
prithiviPM <prithivipemimagar@gmail.com>
2026-01-02 00:56:31 +0545

    feat: add WhyStudySnap component for showcasing StudySnap benefits

---
+++ client/src/components/WhyStudySnap.tsx
@@ -0,0 +1,23 @@
+const WhyStudySnap = () => {
+	return (
+		<section className="min-h-dvh px-2 py-30 sm:py-50 flex flex-col gap-4 md:gap-20">
+			<div className="max-w-5xl mx-auto flex-center flex-col gap-3 sm:gap-8">
+				<p className="font-semibold text-xs text-study-snap-700">
+					Why StudySnap?
+				</p>
+				<div>
+					<h1 className="text-3xl sm:text-5xl md:text-[80px] lg:w-8xl md:leading-20 font-bold text-center">
+						Everything You Need to
+						<br /> <span className="text-study-snap-700">Prepare Smarter</span>
+					</h1>
+				</div>
+				<p className="text-center w-[80%] sm:w-[60%] text-xs sm:text-base md:text-xl">
+					Our AI Tools are designed to save your time and boost your retention
+					by focusing only in the content.
+				</p>
+			</div>
+		</section>
+	);
+};
+
+export default WhyStudySnap;
