eb721018d3f1cfaf1371ab124fb44477637a72f6
prithiviPM <prithivipemimagar@gmail.com>
2026-01-01 18:59:31 +0545

    feat: refactor App component and add Button component with variants

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest/config" />
-
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
